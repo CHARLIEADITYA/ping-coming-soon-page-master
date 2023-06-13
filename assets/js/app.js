2ddaae9bfeb4061e094d03a215469489b98a6706
CHARLIEADITYA <adityabaranwal34@gmail.com>
2023-06-13 13:21:22 +0530

    jschange

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,23 @@
 document.querySelector(".BtnClick").addEventListener("click", () => {
-   let emailvalue = document.querySelector(".emailInput").value;
-    console.log(emailvalue);
-    
+  var emailvalue = document.querySelector(".emailInput").value;
+  console.log(emailvalue);
 
-var mailPattern =
-    /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  var mailPattern =
+     /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
   if (mailPattern.test(emailvalue)) {
     // document.querySelector(".error").classList.remove("showError");
     // document.querySelector(".errorPara").classList.add("hide");
-    document.querySelector(".error_msg").style.display="none"
-    document.querySelector(".emailInput").style.border="1px solid hsl(223, 87%, 63%)"
-} 
-else {
+    document.querySelector(".error_msg").style.display = "none";
+    document.querySelector(".emailInput").style.border =
+      "1px solid hsl(223, 87%, 63%)";
+    document.querySelector(".emailInput").value = "";
+  } else {
     // document.querySelector(".error").classList.add("showError");
     // document.querySelector(".errorPara").classList.remove("hide");
-    document.querySelector(".error_msg").style.display="block" ; 
-    document.querySelector(".emailInput").style.border="1px solid hsl(354, 100%, 66%)"
+    document.querySelector(".error_msg").style.display = "block";
+    document.querySelector(".emailInput").style.border =
+      "1px solid hsl(354, 100%, 66%)";
+    document.querySelector(".emailInput").value = "";
   }
 });
